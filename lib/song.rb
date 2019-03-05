489a2b370293802b295244233f59f650d5abd11a
Brian Simonton <bsimonton78@gmail.com>
2019-03-05 03:56:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,21 +28,9 @@ def initialize(name, artist, genre)
 @@artists << artists
 end 
   
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
   
-  def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
-  end
+  
+  
   
 end 
   
